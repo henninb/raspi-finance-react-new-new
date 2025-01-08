558d8f7a3a6181f74f24bd7a721a2dcb4b6f30cf
Brian Henning <henninb@msn.com>
2025-01-08 08:23:15 -0600

    wip

--- src/components/PaymentTable.tsx
+++ src/components/PaymentTable.tsx
@@ -18,12 +18,32 @@ import usePaymentDelete from "./queries/usePaymentDelete";
 import useFetchParameter from "./queries/useFetchParameter";
 import Payment from "./model/Payment";
 import Transaction from "./model/Transaction";
+import { GridValueFormatter } from '@mui/x-data-grid';
 
 export default function PaymentTable() {
   const [message, setMessage] = useState("");
   const [open, setOpen] = useState(false);
 
   const history = useNavigate();
+
+  const dataTest = [    {
+    "paymentId": 2593,
+    "accountNameOwner": "rcard_brian",
+    "transactionDate": "2024-09-24",
+    "amount": 1.00,
+    "guidSource": "90bea775-5b25-40f2-bc30-ca95a8083c1c",
+    "guidDestination": "4fde7725-1960-42ca-87c0-204040429e6a",
+    "activeStatus": true
+},
+{
+    "paymentId": 2595,
+    "accountNameOwner": "wellsfargo-cash_brian",
+    "transactionDate": "2024-09-24",
+    "amount": 1.00,
+    "guidSource": "feb5fd7f-ca40-4faa-a031-c5fa4f0c02a5",
+    "guidDestination": "c01495ae-46a6-45c1-8de1-6afaebb18673",
+    "activeStatus": true
+}]
 
   const { data, isSuccess } = useFetchPayment();
   const { data: parameterData, isSuccess: parameterSuccess } =
@@ -80,93 +100,102 @@ export default function PaymentTable() {
     {
       field: "transactionDate",
       headerName: "Transaction Date",
-      type: "date",
+      //type: "string",
       width: 180,
       valueGetter: (params: any) => new Date(params.value),
-      renderCell: (params) => moment(params.value).format("YYYY-MM-DD"),
-      editable: true,
-      renderEditCell: (params: any) => (
-        <LocalizationProvider dateAdapter={AdapterMoment}>
-          <DatePicker
-            value={params.value || null}
-            onChange={(newValue: any) =>
-              params.api
-                .getCellEditorInstances()
-                .forEach((editor: any) => editor.setValue(newValue))
-            }
-            slots={{ textField: TextField }}
-          />
-        </LocalizationProvider>
-      ),
+      renderCell: (params) => "2025-01-08",
+      // editable: true,
+      // renderEditCell: (params: any) => (
+      //   <LocalizationProvider dateAdapter={AdapterMoment}>
+      //     <DatePicker
+      //       value={params.value || null}
+      //       onChange={(newValue: any) =>
+      //         params.api
+      //           .getCellEditorInstances()
+      //           .forEach((editor: any) => editor.setValue(newValue))
+      //       }
+      //       slots={{ textField: TextField }}
+      //     />
+      //   </LocalizationProvider>
+      // ),
     },
-    {
-      field: "accountNameOwner",
-      headerName: "Account Name Owner",
-      width: 180,
-      renderCell: (params) => (
-        <Button
-          style={{ fontSize: ".6rem" }}
-          onClick={() => handleButtonClickLink(params.row)}
-        >
-          {params.row.accountNameOwner}
-        </Button>
-      ),
-      renderEditCell: (params) => (
-        <SelectAccountNameOwnerCredit
-          onChangeFunction={params.onChange}
-          currentValue={params.value}
-        />
-      ),
-      cellClassName: "nowrap",
-    },
+    // {
+    //   field: "accountNameOwner",
+    //   headerName: "Account Name Owner",
+    //   width: 180,
+    //   renderCell: (params) => (
+    //     <Button
+    //       style={{ fontSize: ".6rem" }}
+    //       onClick={() => handleButtonClickLink(params.row)}
+    //     >
+    //       {params.row.accountNameOwner}
+    //     </Button>
+    //   ),
+    //   renderEditCell: (params) => (
+    //     <SelectAccountNameOwnerCredit
+    //       onChangeFunction={params.onChange}
+    //       currentValue={params.value}
+    //     />
+    //   ),
+    //   cellClassName: "nowrap",
+    // },
     {
       field: "amount",
       headerName: "Amount",
       type: "number",
       width: 150,
-      valueFormatter: (params: any) =>
-        params.value
-          ? params.value.toLocaleString("en-US", {
-              style: "currency",
-              currency: "USD",
-            })
-          : "",
+      renderCell: (params: any) =>
+        params.value?.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+        }),
+      // valueFormatter: (params: any) => {
+      //     console.log("param-value:" + params.value);
+      //     params.value?.toLocaleString("en-US", {
+      //         style: "currency",
+      //         currency: "USD",
+      //       })
+      // },
       editable: true,
       cellClassName: "nowrap",
     },
-
-    // {
-    //   field: "sourceAccount",
-    //   headerName: "Source Account",
-    //   width: 180,
-    //   editable: true,
-    //   renderEditCell: (params: any) => (
-    //     <SelectAccountNameOwnerDebit
-    //       onChangeFunction={(value: any) =>
-    //         params.api
-    //           .getCellEditorInstances()
-    //           .forEach((editor: any) => editor.setValue(value))
-    //       }
-    //       currentValue={params.value || ""}
-    //     />
-    //   ),
-    // },
-    // {
-    //   field: "destinationAccount",
-    //   headerName: "Destination Account",
-    //   width: 200,
-    //   editable: true,
-    //   renderEditCell: (params: any) => (
-    //     <SelectAccountNameOwnerCredit
-    //       onChangeFunction={(value: any) =>
-    //         params.api
-    //           .getCellEditorInstances()
-    //           .forEach((editor: any) => editor.setValue(value))
-    //       }
-    //       currentValue={params.value || ""}
-    //     />
-    //   ),
-    // },
+    {
+      field: "sourceAccount",
+      headerName: "Source Account",
+      width: 180,
+      editable: true,
+      //renderCell: parameterData?.parameterValue || "none",
+      //renderCell: "amazing",
+      // valueFormatter: (params: any) => {
+      //   parameterData.parameterValue
+      // },
+      renderEditCell: (params: any) => (
+        <SelectAccountNameOwnerDebit
+          onChangeFunction={(value: any) =>
+            params.api
+              .getCellEditorInstances()
+              .forEach((editor: any) => editor.setValue(parameterData.parameterValue))
+          }
+          currentValue={params.value || ""}
+        />
+      ),
+    },
+    {
+      field: "accountNameOwner",
+      headerName: "Destination Account",
+      width: 200,
+      editable: true,
+      renderEditCell: (params: any) => (
+        <SelectAccountNameOwnerCredit
+          onChangeFunction={(value: any) =>
+            params.api
+              .getCellEditorInstances()
+              .forEach((editor: any) => editor.setValue(value))
+          }
+          currentValue={params.value || ""}
+        />
+      ),
+    },
   ];
 
   return (
@@ -177,9 +206,10 @@ export default function PaymentTable() {
           <DataGrid
             columns={columns}
             rows={data}
-            //pagination
-            //checkboxSelection
+            pagination
+            paginationModel={{ pageSize: 40, page: 0 }}
             getRowId={(row:any) => row.paymentId}
+            editMode="cell"
             // sx={{
             //   "& .MuiDataGrid-columnHeaders": {
             //     backgroundColor: "#9965f4",
